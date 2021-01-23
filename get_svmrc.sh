8f326f1b3fc327491ade5fc351965be31fa7d8b2
sam bacha <sam@freighttrust.com>
2021-01-23 01:53:47 -0800

    Create get_svmrc.sh

---
+++ get_svmrc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# solidity version managment 
+# this function just respondes with available versions 
+export LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n
+
+svmrc() {
+curl "https://api.github.com/repos/ethereum/solidity/tags" | jq '.[].name'  
+}
