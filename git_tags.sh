21e8ece54e3575a2464813e16d065e9776f1fec2
sam bacha <sam@freighttrust.com>
2021-04-01 14:54:07 -0700

    fix: markup

--- git_tags.sh
+++ git_tags.sh
@@ -1,3 +1,4 @@
- #!/bin/bash
- # @usage: this command fetches all the tags and removes the hashees
- git ls-remote -t --refs https://github.com/ethereum/solidity | sed -E 's/^[[:xdigit:]]+[[:space:]]+refs\/tags\/(.+)/\1/g'
+#!/bin/bash
+# @usage: this command fetches all the tags and removes the hashees
+git ls-remote -t --refs https://github.com/ethereum/solidity | sed -E 's/^[[:xdigit:]]+[[:space:]]+refs\/tags\/(.+)/\1/g'
+#EOF
