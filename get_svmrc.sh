db210475154528904f400a80b8749a8d03eebf3b
sam bacha <sam@freighttrust.com>
2022-03-20 19:53:34 -0700

    fix sort

--- get_svmrc.sh
+++ get_svmrc.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # solidity version managment 
 # this function just respondes with available versions 
-export LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n
+export LC_ALL=C 
+# sort -t. -k 1,1 -k 2,2n -k 3,3n
 
 svmrc() {
-curl "https://api.github.com/repos/ethereum/solidity/tags" | jq '.[].name'  
+curl "https://api.github.com/repos/ethereum/solidity/tags" | jq '.[].name'  | sort -t. -k 1,1 -k 2,2n -k 3,3n
 }
